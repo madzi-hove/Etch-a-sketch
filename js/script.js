d2df2e50949b6b694682619305734689688df9e5
Madzi hove <madzihove@gmail.com>
2022-11-24 19:18:54 +0200

    Display size of grid underneath the slider

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 //DOM variables
 const menuContainer = document.querySelector(".menu-container");
 const canvas = document.querySelector(".canvas__item");
-const height = canvas.offsetHeight;
-// canvas.style.maxWidth = height + "px";
-console.log(canvas, height);
+const gridSize = document.querySelector("#grid-size");
+const gridSizeDisplay = document.querySelector(".grid-size-display");
+
+let penColour = "";
+gridSizeDisplay.innerText = `16 X 16`;
 
 for (let i = 0; i < 16; i++) {
 	const gridCell = document.createElement("div");
@@ -13,6 +15,17 @@ for (let i = 0; i < 16; i++) {
 
 const menuSelection = function (event) {
 	console.log(event.target);
+	if (event.target.id === "colour-picker") {
+		penColour = event.target.value;
+		console.log(penColour);
+	}
+
+	if (event.target.id === "grid-size") {
+		size = event.target.value;
+		console.log(size);
+		gridSizeDisplay.innerText = `${size} X ${size}`;
+	}
 };
 
-menuContainer.addEventListener("click", (e) => menuSelection(e));
+// menuContainer.addEventListener("click", (e) => menuSelection(e));
+menuContainer.addEventListener("change", (e) => menuSelection(e));
