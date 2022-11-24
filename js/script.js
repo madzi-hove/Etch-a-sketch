d7efef3eab5357ef33806e48dbd85baa16828c6f
Madzi hove <madzihove@gmail.com>
2022-11-24 18:03:17 +0200

    Add event listener to menu container

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+//DOM variables
+const menuContainer = document.querySelector(".menu-container");
+const canvas = document.querySelector(".canvas__item");
+const height = canvas.offsetHeight;
+// canvas.style.maxWidth = height + "px";
+console.log(canvas, height);
 
+for (let i = 0; i < 16; i++) {
+	const gridCell = document.createElement("div");
+	gridCell.classList.add("cell");
+	canvas.append(gridCell);
+}
+
+const menuSelection = function (event) {
+	console.log(event.target);
+};
+
+menuContainer.addEventListener("click", (e) => menuSelection(e));
