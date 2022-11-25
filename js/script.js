cf194c6ad14ce920fab8157810b3e68bab10c5bf
Madzi hove <madzihove@gmail.com>
2022-11-25 16:23:03 +0200

    create grid on window load event

--- js/script.js
+++ js/script.js
@@ -3,19 +3,20 @@ const root = document.querySelector(":root");
 const menuContainer = document.querySelector(".menu-container");
 const canvas = document.querySelector(".canvas__item");
 const gridSize = document.querySelector("#grid-size");
-let gridCell;
 const gridSizeDisplay = document.querySelector(".grid-size-display");
 
+let gridCell;
 let penColour = "";
+let target = "";
 const rootStyles = getComputedStyle(root);
-let size = rootStyles.getPropertyValue("--grid-size");
+let size = +rootStyles.getPropertyValue("--grid-size");
 
-// root.style.setProperty("--grid-size", size);
 gridSizeDisplay.innerText = `${size} X ${size}`;
 
 const createGrid = function (size) {
 	for (let i = 0; i < size ** 2; i++) {
 		gridCell = document.createElement("div");
+		gridCell.setAttribute("id", `cell${i}`);
 		gridCell.classList.add("cell");
 		canvas.append(gridCell);
 	}
@@ -26,21 +27,41 @@ const deleteGrid = function () {
 	list.forEach((element) => element.remove());
 };
 
+const clearGrid = function () {
+	const list = [...document.querySelectorAll(".cell")];
+	list.forEach((element) => (element.style.backgroundColor = "#fff"));
+};
+
+const showGrid = function () {
+	const list = [...document.querySelectorAll(".cell")];
+	list.forEach((element) => element.classList.toggle("show-grid"));
+};
+
 const randomColour = function () {
 	const colours = [];
 	for (let i = 0; i < 3; i++) {
 		colours.push(Math.floor(Math.random() * 256));
 	}
-
-	console.log(`rgb(${colours[0]}, ${colours[1]}, ${colours[2]})`);
+	return `rgb(${colours[0]}, ${colours[1]}, ${colours[2]})`;
 };
 
-const mode = function (event) {};
+const shader = function () {
+	penColour;
+};
 
-const inputChange = function (event) {
+const buttonOptions = function (event) {
+	if (event.target.id === "clear") {
+		clearGrid();
+	}
+	if (event.target.id === "show-grid") {
+		showGrid();
+	}
+	target = event.target.id;
+};
+
+const inputOptions = function (event) {
 	if (event.target.id === "colour-picker") {
 		penColour = event.target.value;
-		console.log(penColour);
 	}
 
 	if (event.target.id === "grid-size") {
@@ -52,6 +73,19 @@ const inputChange = function (event) {
 	}
 };
 
-createGrid(size);
-menuContainer.addEventListener("click", (e) => mode(e));
-menuContainer.addEventListener("change", (e) => inputChange(e));
+window.onload = () => createGrid(size);
+menuContainer.addEventListener("click", (e) => buttonOptions(e));
+menuContainer.addEventListener("change", (e) => inputOptions(e));
+canvas.addEventListener("mouseover", (e) => {
+	if (e.target.id !== "canvas") {
+		switch (target) {
+			case "random-colour":
+				e.target.style.backgroundColor = randomColour();
+				break;
+			case "colour-picker":
+				console.log(penColour);
+				e.target.style.backgroundColor = penColour;
+				break;
+		}
+	}
+});
