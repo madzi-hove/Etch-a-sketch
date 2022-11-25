597d6e9d055e5950b6e78b56e3cc24d36be853c0
Madzi hove <madzihove@gmail.com>
2022-11-25 07:27:46 +0200

    Add delete grid function

--- js/script.js
+++ js/script.js
@@ -1,31 +1,58 @@
 //DOM variables
+const root = document.querySelector(":root");
 const menuContainer = document.querySelector(".menu-container");
 const canvas = document.querySelector(".canvas__item");
 const gridSize = document.querySelector("#grid-size");
+let gridCell;
 const gridSizeDisplay = document.querySelector(".grid-size-display");
 
 let penColour = "";
-gridSizeDisplay.innerText = `16 X 16`;
+const rootStyles = getComputedStyle(root);
+let size = rootStyles.getPropertyValue("--grid-size");
 
-for (let i = 0; i < 16; i++) {
-	const gridCell = document.createElement("div");
-	gridCell.classList.add("cell");
-	canvas.append(gridCell);
-}
+// root.style.setProperty("--grid-size", size);
+gridSizeDisplay.innerText = `${size} X ${size}`;
 
-const menuSelection = function (event) {
-	console.log(event.target);
+const createGrid = function (size) {
+	for (let i = 0; i < size ** 2; i++) {
+		gridCell = document.createElement("div");
+		gridCell.classList.add("cell");
+		canvas.append(gridCell);
+	}
+};
+
+const deleteGrid = function () {
+	const list = [...document.querySelectorAll(".cell")];
+	list.forEach((element) => element.remove());
+};
+
+const randomColour = function () {
+	let colour1, colour2, colour3;
+	const colours = [colour1, colour2, colour3];
+	colours.map((colour) => {
+		colour = Math.floor(Math.random() * 256);
+	});
+	return `rgb(${colours[0]}, ${colours[1]}, ${colours[2]})`;
+};
+
+const mode = function (event) {};
+
+const inputChange = function (event) {
 	if (event.target.id === "colour-picker") {
 		penColour = event.target.value;
 		console.log(penColour);
 	}
 
 	if (event.target.id === "grid-size") {
+		deleteGrid();
 		size = event.target.value;
-		console.log(size);
+		root.style.setProperty("--grid-size", size);
+		console.log(rootStyles.getPropertyValue("--grid-size"));
+		createGrid(size);
 		gridSizeDisplay.innerText = `${size} X ${size}`;
 	}
 };
 
-// menuContainer.addEventListener("click", (e) => menuSelection(e));
-menuContainer.addEventListener("change", (e) => menuSelection(e));
+createGrid(size);
+menuContainer.addEventListener("click", (e) => mode(e));
+menuContainer.addEventListener("change", (e) => inputChange(e));
